5bd166730f62e0b652de2cdac24ea31be49f2e82
Stefano Pirrello <spirrello@liaison.com>
2019-09-07 10:15:15 -0400

    export functions

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"encryption"
+	"fmt"
 )
 
 func main() {
 	fmt.Println("Starting the application...")
-	ciphertext := encrypt([]byte("Hello World"), "password")
+	ciphertext := encryption.Encrypt([]byte("Hello World"), "password")
 	fmt.Printf("Encrypted: %x\n", ciphertext)
-	plaintext := decrypt(ciphertext, "password")
+	plaintext := encryption.Decrypt(ciphertext, "password")
 	fmt.Printf("Decrypted: %s\n", plaintext)
-	encryptFile("sample.txt", []byte("Hello World"), "password1")
-	fmt.Println(string(decryptFile("sample.txt", "password1")))
+	encryption.EncryptFile("sample.txt", []byte("Hello World"), "password1")
+	fmt.Println(string(encryption.DecryptFile("sample.txt", "password1")))
 }
